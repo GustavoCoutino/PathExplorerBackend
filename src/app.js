fff6eab9e9d1a457c4e271b918c310fd28ee1de8
Gustavo Coutiño <gcoutino04@gmail.com>
2025-03-31 13:30:59 -0600

    Erase folder, add error handler

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ const helmet = require("helmet");
 const morgan = require("morgan");
 const dotenv = require("dotenv");
 const auth = require("./middleware/auth");
+const error = require("./middleware/error");
 const authRoutes = require("./routes/authRoutes");
 
 dotenv.config();
@@ -23,22 +24,7 @@ app.use(express.urlencoded({ extended: true }));
 auth.initializePassport(app);
 
 app.use("/api/auth", authRoutes);
-
-app.use((req, res, next) => {
-  res.status(404).json({
-    success: false,
-    message: "Route not found",
-  });
-});
-
-app.use((err, req, res, next) => {
-  console.error(err.stack);
-  res.status(500).json({
-    success: false,
-    message: "Something went wrong!",
-    error: process.env.NODE_ENV === "development" ? err.message : undefined,
-  });
-});
+app.use(error);
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
