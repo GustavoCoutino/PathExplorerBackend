ab3fd29d222f09e1bbd61c1aaaf04da038738abc
Gustavo Coutiño <gcoutino04@gmail.com>
2025-05-14 17:06:09 -0600

    Added autorizations to development

--- src/routes/developmentRoutes.js
+++ src/routes/developmentRoutes.js
@@ -2,43 +2,45 @@ const express = require("express");
 const router = express.Router();
 const developmentController = require("../controllers/developmentController");
 const auth = require("../middleware/auth");
-
 router.get(
   "/all-courses",
   auth.authenticateJWT,
-  auth.authorize("empleado"),
+  auth.authorize(["empleado", "manager"]),
   developmentController.getAllCourses
 );
 
 router.get(
   "/all-certifications",
   auth.authenticateJWT,
-  auth.authorize("empleado"),
+  auth.authorize(["empleado", "manager"]),
   developmentController.getAllCertifications
 );
 
 router.post(
   "/create-course",
   auth.authenticateJWT,
-  auth.authorize("empleado"),
+  auth.authorize(["empleado", "manager"]),
   developmentController.addUserCourse
 );
 
 router.post(
   "/create-certification",
   auth.authenticateJWT,
-  auth.authorize("empleado"),
+  auth.authorize(["empleado", "manager"]),
   developmentController.addUserCertification
 );
+
 router.patch(
   "/edit-certification",
   auth.authenticateJWT,
+  auth.authorize(["empleado", "manager"]),
   developmentController.editUserCertification
 );
 
 router.patch(
   "/edit-course",
   auth.authenticateJWT,
+  auth.authorize(["empleado", "manager"]),
   developmentController.editUserCourse
 );
 
