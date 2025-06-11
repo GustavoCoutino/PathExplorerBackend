0e3afc5697d98a491b61cbc52e88c3e93aa5d1f0
Gustavo Coutiño <gcoutino04@gmail.com>
2025-06-11 00:40:09 -0600

    Remove trailing comma

--- app.js
+++ app.js
@@ -41,7 +41,7 @@ const corsOptions = {
 
     const allowedOriginPatterns = [
       "https://path-explorer-front-end-rogj.vercel.app",
-      "https://path-explorer-front-end-sand.vercel.app/",
+      "https://path-explorer-front-end-sand.vercel.app",
       "http://localhost:3000",
     ];
 
